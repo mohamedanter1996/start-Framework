c8eb4398e8dcd02747e5d920c2a9f9a3ddbbce6d
mohamedanter1996 <mohamedanter461@yahoo.com>
2023-09-18 17:21:20 +0300

    fourth commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path:"",component:HomeComponent,title:"home"},
   {path:"about",component:AboutComponent,title:"about"},
   {path:"contacts",component:ContactsComponent,title:"contacts"},
-  {path:"portofolio",component:PortfolioComponent,title:"portfolio"}
+  {path:"portofolio",component:PortfolioComponent,title:"portfolio"},
+  {path:"**",component:NotFoundComponent,title:"Error 404"}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     ContactsComponent,
     PortfolioComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
